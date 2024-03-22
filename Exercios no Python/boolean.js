551de8326f03d3c7936d553bffd1b256d5c5ee72
daniel-rodri <162440520+daniel-rodri@users.noreply.github.com>
2024-03-22 07:53:46 -0300

    Add files via upload

---
+++ Exercios no Python/boolean.js
@@ -0,0 +1,20 @@
+//Boolean
+//true -> verdadeiro
+//false <- falso
+const PrimeiroNumero = 5;
+const SegundoNumero = 5;
+let CadastroAtivado = true;
+
+//comparação das variaveis com operadores
+
+console.log(PrimeiroNumero === SegundoNumero);//igual;
+console.log(PrimeiroNumero != SegundoNumero);//diferente
+console.log(PrimeiroNumero < SegundoNumero);//menor
+console.log(PrimeiroNumero > SegundoNumero);//maior
+console.log(PrimeiroNumero <= SegundoNumero);//menor ou igual
+console.log(PrimeiroNumero >= SegundoNumero)//maior ou igual
+
+const texto1 = "Alura";
+const texto2 = "Alura";
+
+console.log(texto1 === texto2)
---
+++ Exercios no Python/string.js
@@ -0,0 +1,19 @@
+//Tipo Sting
+
+const texto1 = "Olá mundo!";
+const texto2 = "Olá mundo!";
+const senha = "Senha@superSegura456";
+const StringDenumeros = "34567";
+
+const citacao = "Meu nome é "
+const MeuNome = "Daniel";
+
+//concatenação (+)
+
+console.log (texto1)
+console.log (texto2)
+console.log (senha)
+console.log (StringDenumeros)
+console.log (citacao+MeuNome)
+
+//template string ou template literal
